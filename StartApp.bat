950ebe7c201dc9e65c9d6e72ce18876f223ed803
hasnakhaisa <hasnakhaisa@gmail.com>
2018-10-17 16:55:29 +0700

    move dir

---
+++ StartApp.bat
@@ -0,0 +1,1 @@
+set DEBUG=bea-project:* & npm start
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+router.get('/form-surat', function(req, res, next) {
+    res.render('form-surat', { title: 'Express' });
+});
+
+module.exports = router;
